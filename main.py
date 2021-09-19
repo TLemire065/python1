8da7ac702f22736fe07fe161b574b92eddfe3caf
TLemire065 <tlemire065@g.rwu.edu>
2021-09-18 22:16:47 -0400

    Assignment 2 Completed

--- main.py
+++ main.py
@@ -9,8 +9,30 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
+# A function that prompts the user for a name, number, and word
+def a2_function():
+    # Asks for a name
+    name = input("Enter your name: ")
+    # Prints the inputted name
+    print("Hello,", name)
+
+    # Asks for a number
+    number = float(input("Enter a number: "))
+    # Multiples the inputted number by itself and prints it
+    print(number, "x", number, "=", number * number)
+
+    # Asks for a word
+    word = input("Enter a word: ")
+    #Calculates how many letters are in the words and prints it
+    print(word, "has", len(word), "characters")
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Tyler')
+    # print_hi('Tyler')
+    a2_function()
+
+
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
