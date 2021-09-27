a35385d27339aae25b6790dd169aba14fcd1e065
TLemire065 <tlemire065@g.rwu.edu>
2021-09-26 21:35:37 -0400

    Assignment 3 Completed

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
+from random import randint
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -23,16 +25,57 @@ def a2_function():
 
     # Asks for a word
     word = input("Enter a word: ")
-    #Calculates how many letters are in the words and prints it
+    # Calculates how many letters are in the words and prints it
     print(word, "has", len(word), "characters")
+
+
+def a3_array():
+    array = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+    # Gets array column index
+    for i in range(len(array[0])):
+        # Gets array row index
+        for j in range(len(array)):
+            # Prints array value based on row and column
+            print(array[j][i], end='')
+        print()
+
+
+def a3_coinflip():
+    # Gets user value
+    user_value = int(input("Enter 1 or 0: "))
+    # Generates a random number of 0 or 1
+    random_value = randint(0, 1)
+
+    # Compares users value to generated value
+    if user_value == random_value: print("You Win!")
+    else: print("You Lose!")
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # print_hi('Tyler')
-    a2_function()
 
+    print("Week 1: Name Print\nWeek 2: Creating A Function\nWeek 3: Printing Arrays & Playing Coinflip\n")
 
+    assignment_number = int(input("Enter Week Number: "))
 
+    if assignment_number == 1: print_hi('Tyler')
+    elif assignment_number == 2: a2_function()
+    elif assignment_number == 3:
+        assignment_selection = int(input("Enter 0 for array function or 1 for coinflip function: "))
+        if assignment_selection == 0:
+            a3_array()
+        elif assignment_selection == 1:
+            a3_coinflip()
+        else: print("Invalid Selection")
+    else: print("Invalid Selection")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
