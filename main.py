4e8fa0c276b5f40bcf54d375e5865a25d49e0a38
TLemire065 <tlemire065@g.rwu.edu>
2021-10-25 20:05:06 -0400

    Feature2 Complete

--- main.py
+++ main.py
@@ -2,15 +2,16 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from random import randint
 
-from random import randint, randrange
 
+def feature2():
+    # Gets a random integer between 1 and 5
+    print("A random integer between 1 and 5 is:", randint(1, 5))
 
-def feature2(name):
-    print()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print()
+    feature2()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
