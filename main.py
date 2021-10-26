ef1ecb3670d71cd4d67cdd223b9bde3c87fc283f
TLemire065 <tlemire065@g.rwu.edu>
2021-10-25 20:02:55 -0400

    Feature1 Complete

--- main.py
+++ main.py
@@ -3,14 +3,14 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-from random import randint, randrange
+import platform
 
+def feature1():
+    print("OS Version:", platform.version())
 
-def feature1(name):
-    print()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print()
+    feature1()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
