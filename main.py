c4ea72c0863b8077ad2a809f5ad27eb0ccf9e418
TLemire065 <tlemire065@g.rwu.edu>
2021-10-25 20:10:37 -0400

    Feature3 Complete

--- main.py
+++ main.py
@@ -3,14 +3,15 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-from random import randint, randrange
+from datetime import date
 
 
-def feature3(name):
-    print()
+def feature3():
+    print("Today's date is:", date.today())
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print()
+    feature3()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
