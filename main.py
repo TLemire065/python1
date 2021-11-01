e420be44c0877bae88c28be870b6385302e835c5
TLemire065 <tlemire065@g.rwu.edu>
2021-11-01 01:08:25 -0400

    Strings1 Finished

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 import sys
 
-if __name__ == "__main__":
-    print("Arguments count: ", len(sys.argv))
 
-    for i, args in enumerate(sys.argv):
-        print(f"Argument{i:6}: {args}")
+def string_manipulation1(args):
+    # Combines listed args
+    args = ' '.join(args)
+    # Checks if there are only lowercase letters
+    if args.islower():
+        print("The string only consists of lowercase letters")
+    # Checks if there are only uppercase letters
+    elif args.isupper():
+        print("The string only consists of uppercase letters")
+    else:
+        print("The string consists of lower and uppercase letters")
+
+
+def string_manipulation2(args):
+    # Combines listed args
+    args = ' '.join(args)
+    # Capitalizes string
+    capitalized_string = args.upper()
+    print("String capitalized:", capitalized_string)
+    # Titles string
+    titled_string = args.title()
+    print("String titled:", titled_string)
+
+
+if __name__ == "__main__":
+
+    string_manipulation1(sys.argv)
+    string_manipulation2(sys.argv)
